913d2f8f8830cfd26b3b35d7498b69749af8c886
SliekasSatanistas <milinis@gmail.com>
2016-07-15 22:40:42 +0300

    Started drawing library

--- neopixel_image.py
+++ neopixel_image.py
@@ -8,6 +8,8 @@ imageFormat = 'bmp'
 imageName = 'tstlib.bmp'
 axisGap = 20
 axisSize = 256
+lastPixelX = None
+lastPixelY = None
 
 def saveImage():
     im.save(imageName,  imageFormat)
@@ -21,12 +23,18 @@ def drawAxis():
     draw.text((axisGap-8, axisSize+axisGap-5), '0')
     
 def putAxisPixel(x,  y):
+    #if (lastPixelX is not None) and (lastPixelY is not None):
+    #if lastPixelX is not None:
+    #    draw.line((lastPixelX,  pastPixelY,  x,  y),  (50, 0, 0))
+    lastPixelX = x
+    lastPixelY = y
+    
     if y > (axisSize-1):
         draw.line((axisGap+1+x,  axisGap-2,  axisGap+1+x, axisGap-6))
     
     if y < 0:
         draw.line((axisGap+1+x,  axisSize+axisGap+2,  axisGap+1+x, axisSize+axisGap+6))
- 
+     
     im.putpixel((axisGap+1+x, axisGap+axisSize-1-y), (255,  0, 0))
   #   im.putpixel((20, 20), (0,  255, 0))
   #  im.putpixel((30, 40), (0,  0, 255))
